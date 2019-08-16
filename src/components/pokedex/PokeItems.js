e1e2af6a5633b30a777e6ebd47666690beb565cf
Abbey Masters <abbeykmasters@gmail.com>
2019-08-16 15:04:03 -0700

    color borders

--- src/components/pokedex/PokeItems.js
+++ src/components/pokedex/PokeItems.js
@@ -5,7 +5,7 @@ class PokeItem extends Component {
         const poke = this.props.poke;
 
         return /*html*/`
-            <li>
+            <li style="border: ${poke.color_1} 3px solid">
                 <h2>${poke.pokemon}</h2>
                 <img id="${poke.id}" src="${poke.url_image}">
                 <p>Type: ${poke.type_1}</p>
